0d5fd89ea8edf6cb14c2c640e3196922d358457f
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2026-03-04 05:39:17 +0200

    relocate

--- Sources/OpenJoystickDriverKit/KeyNames.swift
+++ Sources/OpenJoystickDriverKit/KeyNames.swift
@@ -1,10 +1,10 @@
 import Foundation
 
-enum KeyNames {
-  static func name(for code: UInt16) -> String { lookup[code] ?? "Key \(code)" }
+public enum KeyNames {
+  public static func name(for code: UInt16) -> String { lookup[code] ?? "Key \(code)" }
 
   // swiftlint:disable:next closure_body_length
-  static let lookup: [UInt16: String] = [
+  public static let lookup: [UInt16: String] = [
     0: "A", 1: "S", 2: "D", 3: "F", 4: "H", 5: "G", 6: "Z", 7: "X", 8: "C", 9: "V", 11: "B",
     12: "Q", 13: "W", 14: "E", 15: "R", 16: "Y", 17: "T", 18: "1", 19: "2", 20: "3", 21: "4",
     22: "6", 23: "5", 24: "=", 25: "9", 26: "7", 27: "-", 28: "8", 29: "0", 30: "]", 31: "O",
